ee7f79abce915abd08c253c79b3c0b7b9fbf8cdd
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-04-26 18:04:00 -0500

    app/views.py corregido

--- PracticaFlask_MySQL/app/views.py
+++ PracticaFlask_MySQL/app/views.py
@@ -1,3 +1,5 @@
+#app/views.py
+
 from flask import render_template, flash, redirect, url_for, session, request, logging
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
@@ -12,9 +14,9 @@ from flask import Flask,jsonify,send_from_directory
 from marshmallow import Schema, fields
 from datetime import date
 
-#agregar clave secreta
-app.secret_key='secret123'
-#config MySQL
+app.secret_key='secrect123'
+
+# Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] ='Autonoma123*'
@@ -23,6 +25,19 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 #INIT MYSQL
 mysql = MySQL(app)
+
+class RegisterForm(Form):
+        name = StringField('Name', [validators.Length(min=1, max=50)])
+        username = StringField('Username', [validators.Length(min=4,max=25)])
+        email = StringField('Email', [validators.Length(min=6, max=50)])
+        password = PasswordField('Password', [validators.DataRequired(),validators.EqualTo('confirm', message='Password do not match')])
+        confirm = PasswordField('Confirm Password')
+
+# CREATE VALIDATOR FORM FOR ARTICLES
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=100)])
+    body = StringField('Body', [validators.Length(min=1,max=2000)])
+    author = StringField('Author', [validators.Length(min=3, max=100)])
 
 spec = APISpec(
         title='Flask-api-swagger-doc',
@@ -33,48 +48,36 @@ spec = APISpec(
 
 @app.route('/api/swagger.json')
 def create_swagger_spec():
-    return jsonify(spec.to_dict())
+        return jsonify(spec.to_dict())
+
 class ArticleResponseSchema(Schema):
-    id = fields.Int()
-    title = fields.Str()
-    body = fields.Str()
-    author = fields.Str()
-    create_date = fields.Date()
+        id = fields.Int()
+        title = fields.Str()
+        body = fields.Str()
+        author = fields.Str()
+        create_date = fields.Str()
+
 class ArticleListResponseSchema(Schema):
-    article_list = fields.List(fields.Nested(ArticleResponseSchema))
-@app.route('/articlesdocs')
-def article2():
-    arts=d.Articles()
+        article_list = fields.List(fields.Nested(ArticleResponseSchema))
+
+@app.route('/articleDocs')
+def articledoc():
     """
     Get List of Articles
-                ---
-                get:
-                description: Get List of Articles
-                responses:
-                        200:
-                            description: Return an article list
-                            content:
-                                application/json:
-                                  schema: ArticleListResponseSchema
+        ---
+        get:
+            description: Get List of Articles
+            responses:
+                200:
+                    description: Return an article list
+                    content:
+                        application/json:
+                            schema: ArticleListResponseSchema
     """
-    return ArticleListResponseSchema().dump({'article_list':arts})
-with app.test_request_context():
-    spec.path(view=article2)
+    return ArticleListResponseSchema().dump({'article_list':d.Articles()})
 
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4,max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-    validators.DataRequired(),
-    validators.EqualTo('confirm', message='Password do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-# CREATE VALIDATOR FORM FOR ARTICLES
-class ArticleForm(Form):
-    title = StringField('Title', [validators.Length(min=1, max=100)])
-    body = StringField('Body', [validators.Length(min=1,max=2000)])
-    author = StringField('Author', [validators.Length(min=3, max=100)])
+with app.test_request_context():
+        spec.path(view=articledoc)
 
 @app.route('/')
 def index():
@@ -83,78 +86,89 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
-@app.route('/articles')
-def articles():
-    art=len(d.Articles())
-    return render_template("articles.html",data=art)
-@app.route('/article/<int:id>')
-def getArticle(id):
-    articles=d.Articles()
-    for data in articles:
-        if data['id']==id:
-           art=data
-           return render_template("article.html",data=art)
-    return render_template("article.html")
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
-        # Create Cursor
-        cur = mysql.connection.cursor()
-        # Execute Query
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name,email,username,password))
-        # Commit to DB
-        mysql.connection.commit()
-        # Close connection
-        cur.close()
-        flash('You are now registered and can log in', 'success')
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+                name = form.name.data
+                email = form.email.data
+                username = form.username.data
+                password = sha256_crypt.encrypt(str(form.password.data))
+
+                # Create Cursor
+                cur = mysql.connection.cursor()
+                # Execute Query
+                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username,password))
+
+                # Commit to DB
+                mysql.connection.commit()
+
+                # Close connection
+                cur.close()
+
+                flash('You are now registered and can log in','success')
+
+                return redirect(url_for('index'))
+
+        return render_template('register.html',form=form)
+
 # User login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # Get Form Fields
-        username = request.form['username']
-        password_candidate = request.form['password']
-        # Create cursor
-        cur = mysql.connection.cursor()
-        # Get user by username
-        result = cur.execute("SELECT * FROM users WHERE username =%s",[username])
-        if result > 0:
-            data = cur.fetchone()
-            password = data['password']
-            if sha256_crypt.verify(password_candidate, password):
-                session['logged_in'] = True
-                session['username'] = username
-                flash('You are logged in', 'success')
-                app.logger.info('PASSWORD MATCHED')
-                return redirect(url_for('dashboard'))
-            else:
-                app.logger.info('PASSWORD NO MATCHED')
-                error = 'Invalid login'
-                return render_template('login.html',error=error)
-                cur.close()
-        else:
-            app.logger.info('NO USER')
-            error = 'Username not found'
-            return render_template('login.html', error=error)
-    return render_template('login.html')
+        if request.method == 'POST':
+
+                # Get Form Fields
+                username = request.form['username']
+                password_candidate = request.form['password']
+
+                # Create cursor
+                cur = mysql.connection.cursor()
+
+                # Get user by username
+                result = cur.execute("SELECT * FROM users WHERE username =%s",[username])
+
+                if result > 0:
+                        # Get stored hash
+                        data = cur.fetchone()
+                        password = data['password']
+
+                        # Compare Passwords
+                        if sha256_crypt.verify(password_candidate, password):
+                                # Passed
+                                session['logged_in'] = True
+                                session['username'] = username
+                                flash('You are logged in', 'success')
+                                app.logger.info('PASSWORD MATCHED')
+                                return redirect(url_for('dashboard'))
+
+                        else:
+                                app.logger.info('PASSWORD NO MATCHED')
+                                error = 'Invalid login'
+                                return render_template('login.html',error=error)
+
+                        # Close connection
+                        cur.close()
+
+                else:
+                        app.logger.info('NO USER')
+                        error = 'Username not found'
+                        return render_template('login.html', error=error)
+
+        return render_template('login.html')
+
 # Check if user logged in
 def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Plese login', 'danger')
-            return redirect(url_for('login'))
-    return wrap
+        @wraps(f)
+        def wrap(*args, **kwargs):
+                if 'logged_in' in session:
+                        return f(*args, **kwargs)
+                else:
+                        flash('Unauthorized, Plese login','danger')
+                        return redirect(url_for('login'))
+
+        return wrap
+
 # Dashboard
 @app.route('/dashboard')
 @is_logged_in
@@ -164,12 +178,14 @@ def dashboard():
     articles = cur.fetchall()
     cur.close()
     return render_template('dashboard.html', articles=articles)
+
 # Logout
 @app.route('/logout')
 def logout():
-    session.clear()
-    flash('You are now logged out','success')
-    return redirect(url_for('login'))
+        session.clear()
+        flash('You are now logged out','success')
+        return redirect(url_for('login'))
+
 # Add Article
 @app.route('/add_article', methods=['GET', 'POST'])
 def add_article():
@@ -185,6 +201,7 @@ def add_article():
         flash('The article has successfully added!', 'success')
         return redirect(url_for('dashboard'))
     return render_template('add_article.html', form=form)
+
 # Edit Article
 @app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
 def edit_article(id):
@@ -206,6 +223,7 @@ def edit_article(id):
     else:
         return render_template('edit_article.html', form=form, row=row)
     return render_template('edit_article.html', form=form, row=row)
+
 #Delete article
 @app.route('/delete_article/<string:id>', methods=['GET', 'POST'])
 def delete_article(id):
@@ -215,3 +233,27 @@ def delete_article(id):
     cur.close()
     flash('The article has successfully deleted!', 'success')
     return redirect(url_for('dashboard'))
+
+@app.route('/articles')
+def articles():
+    art=len(d.Articles())
+    return render_template("articles.html",data=art)
+
+@app.route('/article/<int:id>')
+def getArticle(id):
+    articles=d.Articles()
+    for data in articles:
+        if data['id']==id:
+            art=data
+            return render_template("article.html",data=art)
+    return render_template("article.html")
+
+
+
+@app.route('/docs')
+@app.route('/docs/<path:path>')
+def swagger_docs(path=None):
+    if not path or path == 'docs.html':
+        return render_template('docs.html',base_url='/docs')
+    else:
+        return send_from_directory('static',path)

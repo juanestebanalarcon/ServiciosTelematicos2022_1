670ecb4be91eb294eff9068c25ea5068b5805c47
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-05-23 21:28:35 -0500

    README.md

--- Evaluacion final/Aprovisionamiento/ScriptFirewall.sh
+++ Evaluacion final/Aprovisionamiento/ScriptFirewall.sh
@@ -1,14 +1,13 @@
-service firewalld start
 service NetworkManager stop
 chkconfig NetworkManager off
-firewall-cmd --zone=dmz --add-interface=eth1 --permanent
-firewall-cmd --zone=dmz --add-service=https --permanent
-firewall-cmd --zone=dmz --add-service=http --permanent
-firewall-cmd --zone=dmz --add-service=ftp --permanent
-firewall-cmd --zone=dmz --add-service=dns --permanent
-firewall-cmd --zone=internal --add-interface=eth2 --permanent
-firewall-cmd --permanent --zone=dmz --add-masquerade
-firewall-cmd --permanent --zone=internal --add-masquerade
-firewall-cmd --permanent --zone="dmz" --add-forward-port=port=8080:proto=tcp:toport=8080:toaddr=192.168.50.3
-firewall-cmd --reload
+sysctl -w net.ipv4.ip_forward=1
 service firewalld start
+chkconfig firewalld on
+firewall-cmd --permanent --zone=public --add-service=http 
+firewall-cmd --permanent --zone=public --add-port=80/tcp 
+firewall-cmd --permanent --zone=public --add-port=8080/tcp 
+firewall-cmd --permanent --zone=public --add-masquerade
+firewall-cmd --permanent --zone=public --add-forward-port=port=80:proto=tcp:toport=8080:toaddr=192.168.50.3
+firewall-cmd --permanent --zone=public --add-forward-port=port=8080:proto=tcp:toport=8080:toaddr=192.168.50.3
+firewall-cmd --permanent --zone=public --add-forward-port=port=443:proto=tcp:toport=8080:toaddr=192.168.50.3
+service firewalld restart
--- Evaluacion final/README.md
+++ Evaluacion final/README.md
@@ -34,3 +34,22 @@ end
 
 end
 ```
+
+### Configuración máquina Firewall:
+
+```bash
+service NetworkManager stop
+chkconfig NetworkManager off
+sysctl -w net.ipv4.ip_forward=1
+service firewalld start
+chkconfig firewalld on
+firewall-cmd --permanent --zone=public --add-service=http 
+firewall-cmd --permanent --zone=public --add-port=80/tcp 
+firewall-cmd --permanent --zone=public --add-port=8080/tcp 
+firewall-cmd --permanent --zone=public --add-masquerade
+firewall-cmd --permanent --zone=public --add-forward-port=port=80:proto=tcp:toport=8080:toaddr=192.168.50.3
+firewall-cmd --permanent --zone=public --add-forward-port=port=8080:proto=tcp:toport=8080:toaddr=192.168.50.3
+firewall-cmd --permanent --zone=public --add-forward-port=port=443:proto=tcp:toport=8080:toaddr=192.168.50.3
+service firewalld restart
+```
+
